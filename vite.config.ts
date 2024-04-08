a196dd0a44b6a12a8f1a381347c784a8299cc807
mi1sh <106558234+mi1sh@users.noreply.github.com>
2024-04-09 01:23:42 +0600

    add vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import {resolve} from 'path';
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths';
 import reactRefresh from '@vitejs/plugin-react-refresh';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), reactRefresh()],
+  plugins: [react(), tsconfigPaths(), reactRefresh(), dts({rollupTypes: true})],
   optimizeDeps: {
     include: ['react', 'react-dom'],
   },
@@ -33,5 +35,11 @@ export default defineConfig({
         }
       }
     }
-  }
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './lib/test/setup.ts',
+    css: true,
+  },
 })
